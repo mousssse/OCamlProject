27778e2b118ff5d7bd2618cb41da472b0d3d5332
Sarah Mousset <sarah.mousset@gmail.fr>
2022-12-22 22:03:36 +0100

    delete an arc from a graph

--- src/tools.ml
+++ src/tools.ml
@@ -15,3 +15,16 @@ let add_arc graph id1 id2 n =
   match find_arc graph id1 id2 with 
   | None -> new_arc graph id1 id2 n
   | Some(label) -> new_arc graph id1 id2 (n + label)
+
+let del_arc graph from_id to_id =
+
+  let create_arc_if_other graph id1 id2 label from_id to_id =
+    if id1 = from_id && id2 = to_id then graph 
+    else new_arc graph id1 id2 label
+  in
+
+  match find_arc graph from_id to_id with 
+  | None -> raise (Graph_error ("No arc from node #" ^ string_of_int from_id ^ " to node #" ^string_of_int to_id))
+  | Some(_) ->
+    let new_graph = clone_nodes graph in 
+    e_fold graph (fun gr id1 id2 lbl -> create_arc_if_other gr id1 id2 lbl from_id to_id) new_graph
--- src/tools.mli
+++ src/tools.mli
@@ -4,3 +4,4 @@ val clone_nodes: 'a graph -> 'b graph
 val clone_nodes_without: id list -> 'a graph -> 'b graph
 val gmap: 'a graph -> ('a -> 'b) -> 'b graph
 val add_arc: int graph -> id -> id -> int -> int graph
+val del_arc: 'a graph -> id -> id -> 'a graph
